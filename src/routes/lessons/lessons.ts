ca5aad3f857f975d16a1b71a34b2e15d22f6b93c
Kevin O'Flaherty <kevin@reveliolabs.com>
2025-02-12 00:02:52 -0500

    Flushed out button interaction a bit more

--- src/routes/lessons/lessons.ts
+++ src/routes/lessons/lessons.ts
@@ -1,7 +1,7 @@
 export const lessons = [
 	{ slug: 'waiting-room', title: 'CSS Workshop' },
 	{ slug: 'centering-things', title: 'Centering Things' },
-	{ slug: 'tic-tac-toe', title: 'Tic Tac Toe' },
-	{ slug: 'button-interactions', title: 'Button Interactions' },
 	{ slug: 'lists-of-things', title: 'Lists of Things' },
+	{ slug: 'button-interactions', title: 'Button Interactions' },
+	{ slug: 'tic-tac-toe', title: 'Tic Tac Toe' },
 ];
